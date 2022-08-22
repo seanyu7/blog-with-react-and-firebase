1f5c1926019a78cd4c77fda7b25d8280f2be7dda
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-08-22 23:50:59 +0900

    ⚡️ make everything as a new to get started

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVxqlv8JFL--SOWGPMSbFH-6RTZc2n47U",
+  authDomain: "blog-with-react-and-fire-92e57.firebaseapp.com",
+  projectId: "blog-with-react-and-fire-92e57",
+  storageBucket: "blog-with-react-and-fire-92e57.appspot.com",
+  messagingSenderId: "710623400670",
+  appId: "1:710623400670:web:0c2fc8fe6eaf70ae85a84f",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app); //この記述はfirebaseを初期化する際に必要となるもので、firebaseのauthentificationを使う際には必要となる。（認証の初期化）
+const provider = new GoogleAuthProvider(); //Google認証のためのプロバイダーを作成する。//インスタンス化というらしい。
+const firestore = getFirestore(app); //firestoreのデータベースを初期化する。
+
+export {auth, provider, db};
