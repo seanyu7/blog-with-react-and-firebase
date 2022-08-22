f1e74e9e526b9646d7da67e190b77e9344ee2a3e
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-08-23 01:15:06 +0900

    💄 used FontAwsomeIcon to make use of Icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
+      <Link to="/">
+        <FontAwesomeIcon icon={faCoffee} />
+        Home
+      </Link>
       <Link to="/createpost">Create a Post</Link>
       <Link to="/login">LogIn</Link>
     </nav>
