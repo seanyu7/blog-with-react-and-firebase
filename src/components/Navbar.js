c2345582b210a116f92498352e1a9b76d0b69e08
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-08-23 19:41:20 +0900

    💄 installing a icon through fortawsomeicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <nav>
       <Link to="/">
-        <FontAwesomeIcon icon={faCoffee} />
+      <FontAwesomeIcon icon={faHouse} />
         Home
       </Link>
       <Link to="/createpost">Create a Post</Link>
