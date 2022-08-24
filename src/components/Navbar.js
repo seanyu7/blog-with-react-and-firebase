5ba262b39fb6a61782e44b9462d570f572819bd1
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-08-25 02:13:15 +0900

    💄 installing icons which used for Nabver compo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHouse,
+  faFilePen,
+  faArrowRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <nav>
       <Link to="/">
-      <FontAwesomeIcon icon={faHouse} />
+        <FontAwesomeIcon icon={faHouse} />
         Home
       </Link>
-      <Link to="/createpost">Create a Post</Link>
-      <Link to="/login">LogIn</Link>
+      <Link to="/createpost">
+        <FontAwesomeIcon icon={faFilePen} />
+        Create a Post
+      </Link>
+      <Link to="/login">
+        <FontAwesomeIcon icon={faArrowRightToBracket} />
+        LogIn
+      </Link>
     </nav>
   );
 };
