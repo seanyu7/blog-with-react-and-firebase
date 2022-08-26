fe2d17fdd180b30b8dd864bec0f6bc2d14cd1db6
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-08-26 23:28:09 +0900

    ✨ ⚡️ make a popup to make a google login feature

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../firebase";
 
 const Login = () => {
-
   const logInWithGoogle = () => {
     //how to make a function of login with google account.
-    
-  }
+    signInWithPopup(auth, provider).then((result) => {
+      console.log(result);
+    });
+  };
 
   return (
     <div>
       <p>Login to get started</p>
       <button onClick={logInWithGoogle}>Login with Google</button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
