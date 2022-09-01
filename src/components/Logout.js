16bb8439ed978cde7a6bdec3c456d6ddc6c635a1
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-09-02 01:36:21 +0900

    ✨ made a logout function

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
-const Logout = () => {
+const Logout = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    //how to make a function of Logout
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      navigate("/login");
+    });
+  };
+
   return (
-    <div>Logout</div>
-  )
-}
+    <div>
+      <p>Logging out</p>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+};
 
-export default Logout
+export default Logout;
