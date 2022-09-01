eef1d3cfeff6148eddbb05b31b40a3d678b5c5ea
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-09-02 02:06:59 +0900

    ✨ created a post component to put up a post

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./createpost.css";
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
-  )
-}
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>creating a post</h1>
+        <div className="inputPost">
+          <div>Title</div>
+          <input type="text" placeholder="Put in your title here" />
+        </div>
+        <div className="inputPost">
+          <div>Tweet</div>
+          <textarea placeholder="Put in your thoughts now"></textarea>
+        </div>
+        <button className="postButton">Tweet</button>
+      </div>
+    </div>
+  );
+};
 
-export default CreatePost
+export default CreatePost;
