174343c4ceb6d0c03ee45bae485874e7b3148879
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-09-02 21:11:09 +0900

    ✨ ⚡️ madeup ahome component for displaying tweets

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import './Home.css'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      <div className='postContents'>
+        <div className='postHeader'>
+          <h1>Title</h1>
+        </div>
+      
+      <div className='postTextContainer'>
+       put some tweet here as your tweet for the day
+      </div>
+      <div className='nameAndDeleteButton'>
+        <h3>@SeanYuTakaki</h3>
+        <button>Delete</button>
+      </div>
+      </div> 
+    </div>
   )
 }
 
