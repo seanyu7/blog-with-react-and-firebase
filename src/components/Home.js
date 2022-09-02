ca53bddd95ddc89bd9e641dbff397323b6633604
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-09-03 02:37:09 +0900

    ✨ madeup a delete button for deleting a tweet

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import { db } from "../firebase";
+import { db , auth} from "../firebase";
 import "./Home.css";
 
 const Home = () => {
@@ -17,27 +17,34 @@ const Home = () => {
     getPost();
   }, []);
 
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "posts", id));
+    window.location.href = "/";
+    //上記の書き方をした場合、削除ボタンを押すことで自走的にページがリロードされることで、データが削除されたことが反映される。
+  };
+
   return (
     <div className="homePage">
       {postList.map((post) => {
         //このreturnは画面上に表示させたい部分を記述する際に使うもので、returnのなかでmap関数などの関数を使って新たに画面に表示させてい場合は、returnのなかでreturnを使うことができる。
         console.log(post);
         return (
-      <div className="postContents" key={post.id}>
-        <div className="postHeader">
-          <h1>{post.title}</h1>
-        </div>
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
 
-        <div className="postTextContainer">
-          {post.postText}
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>@</h3>
-          <button>Delete</button>
-        </div>
-      </div>
-      );
-})};
+            <div className="postTextContainer">{post.postText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@</h3>
+              {post.author.id === auth.currentUser.uid && (
+                <button onClick={() => handleDelete(post.id)}>Delete</button>
+              )}
+            </div>
+          </div>
+        );
+      })}
+      ;
     </div>
   );
 };
