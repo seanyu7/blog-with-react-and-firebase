0d84e0e7aff89c8065207034e909b31f19b72a5f
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-09-02 20:28:00 +0900

    ⚡️ made a title and postText function

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,20 +1,36 @@
 import React from "react";
 import "./createpost.css";
+import { useState } from "react";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState();
+  const [postText, setPostText] = useState();
+
+  const createPost = () => {
+    console.log(title)
+    console.log(postText)
+  }
+
   return (
     <div className="createPostPage">
       <div className="postContainer">
         <h1>creating a post</h1>
         <div className="inputPost">
           <div>Title</div>
-          <input type="text" placeholder="Put in your title here" />
+          <input
+            type="text"
+            placeholder="Put in your title here"
+            onchange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="inputPost">
           <div>Tweet</div>
-          <textarea placeholder="Put in your thoughts now"></textarea>
+          <textarea
+            placeholder="Put in your thoughts now"
+            onchange={(e) => setPostText(e.target.value)}
+          ></textarea>
         </div>
-        <button className="postButton">Tweet</button>
+        <button className="postButton" onClick={createPost}>Tweet</button>
       </div>
     </div>
   );
